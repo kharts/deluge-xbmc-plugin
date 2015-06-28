0fea63147a075e17637cc611108022a56c80d45c
kharts <kharts@ukr.net>
2015-06-28 14:16:59 +0300

    Fix missing States.py file

---
+++ plugin.program.deluge/resources/lib/States.py
@@ -0,0 +1,14 @@
+class StatesEnum(object):
+
+    def __init__(self):
+        self.All = 'All'
+        self.Finished = 'Finished'
+        self.Unfinished = 'Unfinished'
+        self.Unstarted = 'Unstarted'
+        self.Active = 'Active'
+        self.Downloading = 'Downloading'
+        self.Seeding = 'Seeding',
+        self.Paused = 'Paused'
+        self.Queued = 'Queued'
+
+States = StatesEnum()
